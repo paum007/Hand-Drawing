505b22ee8d932bb9d649e373500c06f7d90fd9ab
Pau M. Marcos <paumaria07@gmail.com>
2026-03-18 07:31:33 +0000

    Made it so that someone can choose between one or two hands

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from camera import Camera
+from single_hand import SingleHand
 from gesture_detector import GestureDetector
 
 
 camera = Camera()
 gesture = GestureDetector(camera)
+gesture_single = SingleHand(camera)
 
-gesture.gesture_recognition(camera)
+mode = int(input("Do you want to use [1] hand or [2] hands. Put the number in: "))
+
+if mode == 1: 
+    gesture_single.gesture_recognition(camera)
+
+else:
+    gesture.gesture_recognition(camera)
