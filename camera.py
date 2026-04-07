91031c2f00ec170ac36142f6b22c667387b5b67a
Pau M. Marcos <paumaria07@gmail.com>
2026-04-07 10:06:18 +0100

    Feature: Calculated the live fps

--- camera.py
+++ camera.py
@@ -1,18 +1,24 @@
 import cv2
+import time
 
 class Camera():
     def __init__(self):
         self.feed = cv2.VideoCapture(1)
         self.feed.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-        self.fps = self.feed.set(cv2.CAP_PROP_FPS, 60)
+        self.fps = self.feed.get(cv2.CAP_PROP_FPS)
         self.width = self.feed.get(cv2.CAP_PROP_FRAME_WIDTH)
         self.height = self.feed.get(cv2.CAP_PROP_FRAME_HEIGHT)
         self.timestamp_ms = 0
+        self.live_fps = 0
 
     def frames(self):
 
+        prev_time=time.time()
         self.timestamp_ms += round(1000/self.fps)
         ret, frame = self.feed.read()
+        curr_time = time.time()
+        self.live_fps = round(1/(curr_time - prev_time), 0)
+        prev_time = curr_time
 
         if not ret:
             self.close()
@@ -27,9 +33,6 @@ class Camera():
         # enhanced = cv2.cvtColor(merged, cv2.COLOR_LAB2BGR)
                 
         return cv2.flip(frame, 1), self.timestamp_ms # return enhanced alongside frame if the lighting is a big issue
-    
-    def camera_info(self):
-        return self.fps, 
     
     def close(self):
         self.feed.release()
