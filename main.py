05af34e13c7f2da8262b1c3748922d251bef5d7a
Pau M. Marcos <paumaria07@gmail.com>
2026-04-07 10:06:55 +0100

    Feature: Added a robust input validation

--- main.py
+++ main.py
@@ -7,10 +7,19 @@ camera = Camera()
 gesture = GestureDetector(camera)
 gesture_single = SingleHand(camera)
 
-mode = int(input("Do you want to use [1] hand or [2] hands. Put the number in: "))
+while True:
+    try:
+        mode = int(input("Do you want to use [1] hand or [2] hands. Put the number in: "))
+        if mode in (1, 2):
+            break
+        print("Please enter either 1 or 2")
+    
+    except ValueError:
+        print("Please enter a valid NUMBER")
+
 
 if mode == 1: 
     gesture_single.gesture_recognition(camera)
 
-else:
+elif mode == 2:
     gesture.gesture_recognition(camera)
